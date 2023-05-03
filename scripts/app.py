fd860759c684ee7f12c78ff0be593463fe83e48f
bradley-pearson6597 <44512815+bradley-pearson6597@users.noreply.github.com>
2023-05-03 18:13:17 +0100

    Better way of getting data
    Found a better way to get the json data.

--- scripts/app.py
+++ scripts/app.py
@@ -1,6 +1,8 @@
 import gradio as gr
 import pandas as pd
-from requests_html import HTMLSession
+import requests
+import re
+import json
 
 english_dict = pd.read_csv("dictionary.txt",
                          header = None, 
@@ -8,6 +10,8 @@ english_dict = pd.read_csv("dictionary.txt",
                         names = ['word'])
 english_dict = english_dict.reset_index(drop = True)
 english_dict = english_dict.dropna()
+
+url = 'https://spellbee.org'
 def spell_bee_solver(no_centre, centre):
     full_set = set(no_centre.lower() + centre.lower())
     spell_bee_solver = english_dict[english_dict['word'].str.contains(str(centre.lower()), regex = False)]
@@ -25,19 +29,17 @@ def spell_bee_solver(no_centre, centre):
     final_word_df = final_word_df.sort_values('word_length', ascending = False)
     return(final_word_df)
 
-def get_todays_answers():
-    url = 'https://spellbee.org/'
-    with HTMLSession() as session:
-        page = session.get(url)
-        valid_words = page.html.render(script = 'game.validWords')
-
+def get_spellbee_answers(x):
+    content = requests.get(url)._content
+    content = re.sub(".*window.game = ", "", str(content))
+    content = re.sub("(.*?)\\;.*", "\\1", content)
+    content = json.loads(content)
+    valid_words = content['data']['dictionary']
     final_word_df = pd.DataFrame(valid_words, columns = ['word'])
     final_word_df['word_length'] = final_word_df['word'].str.len()
     final_word_df = final_word_df[final_word_df['word_length'] > 3]
     final_word_df = final_word_df.sort_values('word_length', ascending = False)
     return(final_word_df)
-
-    
 
 with gr.Blocks() as app:
     with gr.Row():
@@ -49,6 +51,6 @@ with gr.Blocks() as app:
     with gr.Row():
         output_df = gr.DataFrame(headers = ['word', 'word_length'])
     solve_button.click(spell_bee_solver, inputs = [no_centre, centre], outputs = [output_df])
-    get_today_answers.click(get_todays_answers, inputs = None, outputs = [output_df])
+    get_today_answers.click(get_spellbee_answers, inputs = [no_centre], outputs = [output_df])
 
 app.launch(debug = True, share = False)
